9199c67eb7e509610b2a5fe3428044ccc9bcdd22
praveern <pnarwelkar@gmail.com>
2019-06-09 16:30:05 -0400

    Fixed issues

--- basic_data_structures/array.py
+++ basic_data_structures/array.py
@@ -11,14 +11,15 @@ class Array(object):
     """
     def __init__(self, ndim=1, mdim=1):
         """
-        The array template could be a single vector array or a N * M array. Internally the structure is a list of lists, except that the length is fixed.
+        The array template could be a single vector array or a N * M array.
+        Internally the structure is a list of lists, except that the length is
+        fixed.
 
         :param ndim: Row dimension of the array. Default set to 1.
         :param mdim: Column dimension of the array. Default set to 1.
         """
         self.ndim = ndim
         self.mdim = mdim
-        
+
     def check_length(self):
         pass
-    
